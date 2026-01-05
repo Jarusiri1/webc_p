79761f6f70337d42957cbac494c50ea9ee13d07b
PEA\513805 <Narintip.Nin@pea.co.th>
2026-01-05 13:28:52 +0700

    update2

--- obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6d33e0b2518c847079cd4862a5cb451fee6e27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec1810503ea51138b03fc87be23b480cf94a9f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
